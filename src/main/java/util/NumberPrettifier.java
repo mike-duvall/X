6750cb91e8ac5bd5976b485d23f214dd4bf35d15
Mike DuVall <mike.a.duvall@gmail.com>
2013-08-19 07:04:24 -0500

    Added failing test to test CI unit test reporting

---
+++ src/main/java/util/NumberPrettifier.java
@@ -0,0 +1,7 @@
+package util;
+
+public class NumberPrettifier {
+    public String prettify(int i) {
+        throw new RuntimeException("Method not yet implemented");
+    }
+}
--- src/test/java/util/NumberPrettifierTest.java
+++ src/test/java/util/NumberPrettifierTest.java
@@ -1,4 +1,27 @@
 package util;
 
+import org.junit.Test;
+import static org.fest.assertions.api.Assertions.*;
+
 public class NumberPrettifierTest {
+
+
+    @Test
+    public void shouldPrettifySmallNumber() {
+        // Given
+        NumberPrettifier numberPrettifier = new NumberPrettifier();
+
+
+        // When
+        String prettifiedNumber = numberPrettifier.prettify(532);
+
+
+        // Then
+        String expectedPrettifiedNumber = "532";
+        assertThat(prettifiedNumber).isEqualTo(expectedPrettifiedNumber);
+    }
 }
+
+
+
+
