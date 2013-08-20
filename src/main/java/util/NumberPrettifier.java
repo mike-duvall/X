1e316dea2477ccb53a0b8d2328595e43f86cdc6a
Mike DuVall <mike.a.duvall@gmail.com>
2013-08-20 07:06:47 -0500

    Refactoring:  Fixed checkstyle errors.  Reduced method length.  Simplified.  Decided to bump max acceptable method length to 15.  Happy to discuss why.

--- src/main/java/util/NumberPrettifier.java
+++ src/main/java/util/NumberPrettifier.java
@@ -5,23 +5,32 @@ public class NumberPrettifier {
 
 
     // Note:  Right now I'm using strict TDD
-    // and just making the tests pass
-    // So clearly this implementation will quickly change
-    // since it's hard-coded
+    // so some of this will be hard-coded until
+    // I get to a test that forces otherwise
     public String prettify(Double aNumber) {
         if( aNumber > 999999) {
-            String stringNumber = aNumber.toString();
-            char  firstDigit = stringNumber.charAt(0);
-            char  secondDigit = stringNumber.charAt(1);
-            if( secondDigit == '0') {
-                return "1M";
-            }
-            else {
-                return "" + firstDigit + "." + secondDigit + "M";
-            }
+            return handleNumbersOverSixDigits(aNumber);
         }
         else {
-            return new Integer(aNumber.intValue()).toString();
+            return handleNumbersSixDigitsOrUnder(aNumber);
+        }
+    }
+
+
+    private String handleNumbersSixDigitsOrUnder(Double aNumber) {
+        return new Integer(aNumber.intValue()).toString();
+    }
+
+
+    private String handleNumbersOverSixDigits(Double aNumber) {
+        String stringNumber = aNumber.toString();
+        char  firstDigit = stringNumber.charAt(0);
+        char  secondDigit = stringNumber.charAt(1);
+        if( secondDigit == '0') {
+            return "1M";
+        }
+        else {
+            return "" + firstDigit + "." + secondDigit + "M";
         }
     }
 
